3f7832bac3ca042e9d8d63a03b014022d0093f41
SanthoshKumar9618 <133628876+SanthoshKumar9618@users.noreply.github.com>
2024-04-17 11:47:32 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -24,35 +24,35 @@ def predict(Buying, Maint, Doors, Persons, Lug_boot, Safety):
     safety = 3
   df =pd.DataFrame([Buying, Maint, Doors, Persons, Lug_boot, Safety]),
       columns=['buying','maint','doors','persons','persons','lug_boot','safty'])
-prediction =model.predict([Buying, Maint, Doors, Persons, Lug_boot, Safety])
-return prediction
+  prediction =model.predict([Buying, Maint, Doors, Persons, Lug_boot, Safety])
+  return prediction
 
 st.title('car Evalution Classification')
 st.image("""https://images.pexels.com/photos/14823597/pexels-photo-14823597.jpeg""")
 
- st.header('Enter the Information of the Car:')
+st.header('Enter the Information of the Car:')
 
- st.text("vhigh = 1 high = 2 med = 3 low = 4")
+st.text("vhigh = 1 high = 2 med = 3 low = 4")
 
- Buying = st.number_input('buying:', min_value=1, max_value=4, value=1)
+Buying = st.number_input('buying:', min_value=1, max_value=4, value=1)
 
- st.text("vhigh = 1 high = 2 med 3 low = 4")
+st.text("vhigh = 1 high = 2 med 3 low = 4")
 
- Maint = st.number_input('maint:', min_value=1, max_value=4, value=1)
+Maint = st.number_input('maint:', min_value=1, max_value=4, value=1)
 
- st.text("2-Doors = 1 3-Doors 2 4-Doors = 3 5more = 4")
+st.text("2-Doors = 1 3-Doors 2 4-Doors = 3 5more = 4")
 
- Doors st.number_input('doors:', min_value=1, max_value=4, value=1)
+Doors st.number_input('doors:', min_value=1, max_value=4, value=1)
 
- st.text("2-persons = 1 4-persons = 2 more = 3 ")
+st.text("2-persons = 1 4-persons = 2 more = 3 ")
 
- Persons st.number_input('persons:', min_value=1, max_value=3, value=1)
+Persons st.number_input('persons:', min_value=1, max_value=3, value=1)
 
- st.text("small 1 med 2 big = 3")
+st.text("small 1 med 2 big = 3")
 
- Lug_boot st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
+Lug_boot st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
 
- Safety st.radio('safety:', ('med', 'high', 'low'))
+Safety st.radio('safety:', ('med', 'high', 'low'))
 
 if st.button('submit_car_Infos'):
   cal_eval=predict(Buying, Maint, Doors, Persons, Lug_boot, Safety)
